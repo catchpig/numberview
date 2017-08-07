c23ef56cdb7429e28ad0ae7630bb9f931c660add
tao <327785185@qq.com>
2017-08-07 17:28:30 +0800

    增加函数

--- app/src/main/java/com/zhuazhu/main/MainActivity.java
+++ app/src/main/java/com/zhuazhu/main/MainActivity.java
@@ -2,37 +2,57 @@ package com.zhuazhu.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.zhuazhu.numberview.NumberView;
 
 public class MainActivity extends AppCompatActivity {
     private NumberView mNumberView;
+    private TextView textView;
+    private EditText maxlength;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.money);
         mNumberView = (NumberView) findViewById(R.id.number);
-        mNumberView.setNumberTextSize(20);
+        maxlength = (EditText) findViewById(R.id.maxlengthEdit);
+        mNumberView.setMaxLength(9);
+//        mNumberView.setNumberDecimalPoint(true);
+        mNumberView.setOnMoneyListener(new NumberView.OnMoneyListener() {
+            @Override
+            public void onMoneyValue(String money) {
+                textView.setText(money);
+            }
+        });
 //        mNumberView.setCustomTextVisible(View.VISIBLE);
 //        mNumberView.setCustomText("清除");
 //        mNumberView.setCustomTextSize(15);
-        mNumberView.setOnNumberListener(new NumberView.OnNumberListener() {
+//        mNumberView.setOnNumberListener(new NumberView.OnNumberListener() {
+//            @Override
+//            public void onNumber(String txt) {
+//                Toast.makeText(MainActivity.this,txt,Toast.LENGTH_LONG).show();
+//            }
+//        });
+//        mNumberView.setOnBackSpaceListener(new NumberView.OnBackSpaceListener() {
+//            @Override
+//            public void onBackSpace() {
+//                Toast.makeText(MainActivity.this,"退格",Toast.LENGTH_LONG).show();
+//            }
+//        });
+//        mNumberView.setOnCustomListener(new NumberView.OnCustomListener() {
+//            @Override
+//            public void onCustom() {
+//                Toast.makeText(MainActivity.this,"自定义控件",Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+        findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onNumber(String txt) {
-                Toast.makeText(MainActivity.this,txt,Toast.LENGTH_LONG).show();
-            }
-        });
-        mNumberView.setOnBackSpaceListener(new NumberView.OnBackSpaceListener() {
-            @Override
-            public void onBackSpace() {
-                Toast.makeText(MainActivity.this,"退格",Toast.LENGTH_LONG).show();
-            }
-        });
-        mNumberView.setOnCustomListener(new NumberView.OnCustomListener() {
-            @Override
-            public void onCustom() {
-                Toast.makeText(MainActivity.this,"自定义控件",Toast.LENGTH_LONG).show();
+            public void onClick(View view) {
+                mNumberView.setMaxLength(Integer.valueOf(maxlength.getText().toString().trim()));
             }
         });
     }
--- library/src/main/java/com/zhuazhu/numberview/NumberView.java
+++ library/src/main/java/com/zhuazhu/numberview/NumberView.java
@@ -2,6 +2,7 @@ package com.zhuazhu.numberview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -11,12 +12,13 @@ import android.widget.TextView;
 /**
  * 创建时间:2017/7/27 14:54<br/>
  * 创建人: 李涛<br/>
- * 修改人: 李涛<br/>
- * 修改时间: 2017/7/27 14:54<br/>
- * 描述:
+ * 修改人: 廖斌<br/>
+ * 修改时间: 2017/8/4 <br/>
+ * 描述: 删除键长按清空，初始化 OnNumber,OnCustum,OnBack，如覆盖，请重新set.OnMoney失效
+ *       不覆盖OnNumber,OnCustum,OnBack，需setOnMoney
  */
 
-public class NumberView extends LinearLayout implements View.OnClickListener{
+public class NumberView extends LinearLayout implements View.OnClickListener,View.OnLongClickListener{
     private TextView mOne;
     private TextView mTwo;
     private TextView mThree;
@@ -31,6 +33,21 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
     private ImageView mBackspaceImg;
     private LinearLayout mUnknown;
     private TextView mUnknownTxt;
+
+    /**
+     * 最大数额位数，5位即99999.99
+     */
+    private int maxLength = 5;
+
+    /**
+     * 是否为整数 默认为false
+     */
+    private boolean isDecimalPoint = false;
+
+    /**
+     * 数字键盘输入金额值
+     */
+    private String moneyValue = "";
     public NumberView(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.view_number,this);
@@ -76,6 +93,7 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
         mEight.setOnClickListener(this);
         mNine.setOnClickListener(this);
         mZero.setOnClickListener(this);
+        mBackspace.setOnLongClickListener(this);
 
         //退格监听
         mBackspace.setOnClickListener(new OnClickListener() {
@@ -103,16 +121,17 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
      * @param color
      */
     public void setCustomBackgroudColor(int color){
-
         mUnknown.setBackgroundColor(color);
     }
 
     /**
      * 设置自定义按钮背景颜色
      * @param colreRes
+     * @deprecated 改变自定义按钮文字和设置自定义按钮的图片会将键盘改为整数键盘，如需小数，请手动更改
      */
     public void setCustomBackgroudResource(int colreRes){
         mUnknown.setBackgroundResource(colreRes);
+        isDecimalPoint = true;
     }
     /**
      * 设置自定义按钮显示/隐藏
@@ -129,10 +148,21 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
     /**
      *  设置定义按钮的文字
      * @param text
+     * @deprecated 改变自定义按钮文字和设置自定义按钮的图片会将键盘改为整数键盘，如需小数，请手动更改
      */
     public void setCustomText(String text){
         mUnknownTxt.setText(text);
+        isDecimalPoint = true;
+    }
 
+    /**
+     * 设置是否为整数
+     * 改变自定义按钮文字和设置自定义按钮的图片会将键盘改为整数键盘，如需小数，请手动更改
+     * @param decimalPoint
+     *
+     */
+    public void setNumberDecimalPoint(boolean decimalPoint){
+        isDecimalPoint = decimalPoint;
     }
 
     /**
@@ -153,18 +183,44 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
     /**
      * 设置数字字体大小
      * @param textSize
+     * @deprecated 大小为SP
      */
     public void setNumberTextSize(float textSize){
-        mOne.setTextSize(textSize);
-        mTwo.setTextSize(textSize);
-        mThree.setTextSize(textSize);
-        mFour.setTextSize(textSize);
-        mFive.setTextSize(textSize);
-        mSix.setTextSize(textSize);
-        mSeven.setTextSize(textSize);
-        mEight.setTextSize(textSize);
-        mNine.setTextSize(textSize);
-        mZero.setTextSize(textSize);
+        mOne.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+        mTwo.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+        mThree.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+        mFour.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+        mFive.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+        mSix.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+        mSeven.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+        mEight.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+        mNine.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+        mZero.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+    }
+
+    /**
+     * 设置字体大小
+     * @param unit 大小单位 TypedValue.SP_DP_PX
+     * @param textSize 字体大小
+     * @deprecated  已隔绝其他单位
+     */
+    public void setNumberTextSize(int unit , float textSize){
+        if(unit == TypedValue.COMPLEX_UNIT_SP || unit == TypedValue.COMPLEX_UNIT_DIP  || unit == TypedValue.COMPLEX_UNIT_PX) {
+            mOne.setTextSize(unit, textSize);
+            mTwo.setTextSize(unit, textSize);
+            mThree.setTextSize(unit, textSize);
+            mFour.setTextSize(unit, textSize);
+            mFive.setTextSize(unit, textSize);
+            mSix.setTextSize(unit, textSize);
+            mSeven.setTextSize(unit, textSize);
+            mEight.setTextSize(unit, textSize);
+            mNine.setTextSize(unit, textSize);
+            mZero.setTextSize(unit, textSize);
+        }
+    }
+
+    public void setMaxLength(int length){
+        this.maxLength = length;
     }
 
     /**
@@ -174,7 +230,12 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
     public void setBackspaceImage(int mipmap){
         mBackspaceImg.setImageResource(mipmap);
     }
-    private OnNumberListener mOnNumberListener;
+    private OnNumberListener mOnNumberListener = new OnNumberListener() {
+        @Override
+        public void onNumber(String number) {
+            updateMoneyValue(number,1);
+        }
+    };
 
     /**
      * 监听数字键盘
@@ -185,7 +246,12 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
     }
 
 
-    private OnBackSpaceListener mOnBackSpaceListener;
+    private OnBackSpaceListener mOnBackSpaceListener = new OnBackSpaceListener() {
+        @Override
+        public void onBackSpace() {
+            updateMoneyValue("删除",11);
+        }
+    };
 
     /**
      * 监听退格
@@ -195,7 +261,14 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
         this.mOnBackSpaceListener = listener;
     }
 
-    private OnCustomListener mOnCustomListener;
+    private OnCustomListener mOnCustomListener = new OnCustomListener() {
+        @Override
+        public void onCustom() {
+            if(!isDecimalPoint) {
+                updateMoneyValue(".", 12);
+            }
+        }
+    };
 
     /**
      * 监听自定义按钮
@@ -203,6 +276,16 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
      */
     public void setOnCustomListener(OnCustomListener listener){
         this.mOnCustomListener = listener;
+    }
+
+    private OnMoneyListener moneyListener;
+
+    /**
+     *  监听金额输入
+     * @param listener
+     */
+    public void setOnMoneyListener(OnMoneyListener listener){
+        this.moneyListener = listener;
     }
 
     @Override
@@ -235,6 +318,76 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
         }
     }
 
+    @Override
+    public boolean onLongClick(View view) {
+        if(view.getId() == R.id.backspace){
+            updateMoneyValue("清空",13);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 根据传递的值来改变 金额
+     * @param code 标签 13-清空 11删除 12小数点
+     * @param title 数额
+     */
+    private void updateMoneyValue(String title,int code){
+        if(moneyValue.contains(".")){
+            if(code == 12){
+                return;
+            }
+
+            if(code < 10) {
+                if (".".equals(String.valueOf(moneyValue.charAt(moneyValue.length() - 1)))) {
+                    moneyValue = moneyValue + title;
+                } else if(".".equals(String.valueOf(moneyValue.charAt(moneyValue.length() - 2)))){
+                    moneyValue = moneyValue + title;
+                } else if(".".equals(String.valueOf(moneyValue.charAt(moneyValue.length() - 3)))){
+
+                }
+                moneyListener.onMoneyValue(moneyValue);
+                return;
+            }
+        }
+        if("0".equals(moneyValue)){
+            if(code == 11){
+                return;
+            }
+            if(code == 13){
+                return;
+            }
+            if(code == 12){
+                moneyValue = moneyValue + title;
+            }else {
+                moneyValue = title;
+            }
+        }else if(code == 11){
+            if(moneyValue.length() == 1){
+                if("0".equals(moneyValue)){
+                    return;
+                }
+                moneyValue = "0";
+            }else {
+                moneyValue = moneyValue.substring(0, moneyValue.length() - 1);
+                if (".".equals(String.valueOf(moneyValue.charAt(moneyValue.length() - 1)))) {
+                    moneyValue = moneyValue.substring(0, moneyValue.length() - 1);
+                }
+            }
+        }else if(code == 13){
+            moneyValue = "0";
+        } else if(moneyValue.length() >= maxLength){
+            if(!moneyValue.contains(".")){
+                if(code == 12) {
+                    moneyValue = moneyValue + title;
+                }
+            }
+        } else {
+            moneyValue = moneyValue + title;
+        }
+        moneyListener.onMoneyValue(moneyValue);
+    }
+
     /**
      * 左下角自定义按钮监听
      */
@@ -253,5 +406,11 @@ public class NumberView extends LinearLayout implements View.OnClickListener{
      */
     public interface OnNumberListener{
         void onNumber(String number);
+    }
+    /**
+     * 金额回调
+     */
+    public interface OnMoneyListener{
+        void onMoneyValue(String money);
     }
 }
